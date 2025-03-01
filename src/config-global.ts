b3ff239a69768a3dcff6c138e7d0b78dd3561af3
Love Dev <lovedev0829@gmail.com>
2025-03-01 15:35:48 +0900

    deploy

--- src/config-global.ts
+++ src/config-global.ts
@@ -1,6 +1,6 @@
 import { paths } from 'src/routes/paths';
 
-export const SERVER_URL = 'http://localhost:5000';
+export const SERVER_URL = 'https://salad-transcription.onrender.com';
 
 // ROOT PATH AFTER LOGIN SUCCESSFUL
 export const PATH_AFTER_LOGIN = paths.dashboard.root; // as '/dashboard'
